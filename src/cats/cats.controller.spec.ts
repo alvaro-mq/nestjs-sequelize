075643095467396750c616e9cabf6fe729127783
alvaro <almamani@agetic.gob.bo>
2021-01-12 14:13:08 -0400

    test a controllers y services

--- src/cats/cats.controller.spec.ts
+++ src/cats/cats.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CatsController } from './cats.controller';
+import { CatsService } from './cats.service';
+
+const testCat = { id: 1, name: 'peluza', age: 5, breed: 'angora' };
 
 describe('CatsController', () => {
   let controller: CatsController;
@@ -7,12 +10,29 @@ describe('CatsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CatsController],
+      providers: [
+        {
+          provide: CatsService,
+          useValue: {
+            getCats: jest.fn(() => [testCat]),
+            addCat: jest.fn(() => testCat),
+          }
+        }
+      ]
     }).compile();
 
     controller = module.get<CatsController>(CatsController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should get new cats', async () => {
+    expect(await controller.getCats()).toEqual([testCat]);
+  });
+
+  it('should make a new cat', async () => {
+    expect(await controller.newCat({
+      name: 'peluza',
+      age: 5,
+      breed: 'angora',
+    })).toEqual(testCat);
   });
 });
--- src/cats/cats.service.spec.ts
+++ src/cats/cats.service.spec.ts
@@ -1,18 +1,39 @@
+import { getModelToken } from '@nestjs/sequelize';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CatsService } from './cats.service';
+import { Cat } from './cat.model'
+
+const testCat = { name: 'peluza', age: 5, breed: 'angora'};
 
 describe('CatsService', () => {
   let service: CatsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CatsService],
+      providers: [
+        CatsService,
+        {
+          provide: getModelToken(Cat),
+          useValue: {
+            findAll: jest.fn(() => [testCat]),
+            create: jest.fn(() => testCat),
+          }
+        }
+      ],
     }).compile();
 
     service = module.get<CatsService>(CatsService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should get the cats', async () => {
+    expect(await service.getCats()).toEqual([testCat]);
   });
+
+  it('should add a cat', async () => {
+    expect(await service.addCat({
+      name: 'peluza',
+      age: 5,
+      breed: 'angora'
+    })).toEqual(testCat);
+  })
 });
