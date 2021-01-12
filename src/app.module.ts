c47d45a4b85e319eb50e2adad9bb6802b701f91d
alvaro <almamani@agetic.gob.bo>
2021-01-12 12:45:47 -0400

    integracion de sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'cats_db',
+      synchronize: true,
+      autoLoadModels: true,
+    }),
+    CatsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/cats/cats.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { CatsController } from './cats.controller';
+import { CatsService } from './cats.service';
+
+import { Cat } from './cat.model';
+
+@Module({
+  imports: [SequelizeModule.forFeature([Cat])],
+  controllers: [CatsController],
+  providers: [CatsService],
+})
+export class CatsModule {}
