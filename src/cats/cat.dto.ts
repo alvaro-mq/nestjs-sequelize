a8cf046c3192c248c66ebefc5e358d43d62a465d
alvaro <almamani@agetic.gob.bo>
2021-01-12 15:15:14 -0400

    documentacion para rutas swagger

--- src/cats/cat.dto.ts
+++ src/cats/cat.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CatDTO {
+  @ApiProperty()
   name: string;
+
+  @ApiProperty()
   age: number;
+
+  @ApiProperty()
   breed: string;
 }
