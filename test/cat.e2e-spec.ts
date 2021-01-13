07955e23a601c285d758e79f80f79ce624e55983
alvaro <almamani@agetic.gob.bo>
2021-01-13 11:24:17 -0400

    adicion de test e2e

---
+++ test/cat.e2e-spec.ts
@@ -0,0 +1,39 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, HttpStatus } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { CatDTO } from 'src/cats/cat.dto';
+
+describe('E2E Tests for NOTE Endpoints', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    jest.setTimeout(10000);
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('should create a cat', () => {
+    const cat: CatDTO = {
+      name: 'peluza',
+      age: 5,
+      breed: 'other',
+    };
+    return request(app.getHttpServer())
+      .post('/cats')
+      .set('Accept', 'application/json')
+      .send(cat)
+      .expect(HttpStatus.CREATED);
+  });
+
+  it('should get all cats', () => {
+    return request(app.getHttpServer())
+      .get('/cats')
+      .set('Accept', 'application/json')
+      .expect(HttpStatus.OK);
+  });
+});
